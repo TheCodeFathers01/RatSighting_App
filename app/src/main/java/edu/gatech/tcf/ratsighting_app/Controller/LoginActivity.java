2256b6c817683d190a9675012023e72c8a6940c0
rbhat35 <rsbhat2@gmail.com>
2017-10-01 19:43:07 -0400

    Fixed google services JSON

--- app/src/main/java/edu/gatech/tcf/ratsighting_app/Controller/LoginActivity.java
+++ app/src/main/java/edu/gatech/tcf/ratsighting_app/Controller/LoginActivity.java
@@ -2,12 +2,14 @@ package edu.gatech.tcf.ratsighting_app.Controller;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TextInputEditText;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -59,6 +61,8 @@ public class LoginActivity extends AppCompatActivity {
         });
 
         mAuth = FirebaseAuth.getInstance();
+
+
     }
 
     private void goToPostLogin() {
