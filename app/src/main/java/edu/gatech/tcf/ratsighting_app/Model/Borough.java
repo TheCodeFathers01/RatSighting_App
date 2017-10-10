50eaebac8b8ee50bb842c5d43bb2c6f4316b5573
Tate <tdavenport34>
2017-10-10 13:30:27 -0400

    Added csv parsing libaries

--- app/src/main/java/edu/gatech/tcf/ratsighting_app/Model/Borough.java
+++ app/src/main/java/edu/gatech/tcf/ratsighting_app/Model/Borough.java
@@ -1,5 +1,6 @@
 package edu.gatech.tcf.ratsighting_app.Model;
 
+
 public enum Borough {
     MANHATTAN("MANHATTAN"), STATENISLAND("STATEN ISLAND"), QUEENS("QUEENS"), BROOKLYN("BROOKLYN"), BRONX("BRONX");
 
