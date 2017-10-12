0292d3c44b8ff37a21e1410ee233d8e94fb0dc05
rbhat35 <rsbhat2@gmail.com>
2017-10-11 20:14:03 -0400

    Added rat sighting report activity

---
+++ app/src/main/java/edu/gatech/tcf/ratsighting_app/Controller/AddNewRatSightingReport.java
@@ -0,0 +1,31 @@
+package edu.gatech.tcf.ratsighting_app.Controller;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import edu.gatech.tcf.ratsighting_app.R;
+
+public class AddNewRatSightingReport extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_new_rat_sighting_report);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
