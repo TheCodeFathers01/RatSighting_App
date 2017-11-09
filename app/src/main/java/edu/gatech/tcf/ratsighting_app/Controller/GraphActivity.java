de7c4e5c4a0d20e1dce9cce116deec7f8005692d
rbhat35 <rsbhat2@gmail.com>
2017-11-08 20:17:27 -0500

    Labels created

--- app/src/main/java/edu/gatech/tcf/ratsighting_app/Controller/GraphActivity.java
+++ app/src/main/java/edu/gatech/tcf/ratsighting_app/Controller/GraphActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.jjoe64.graphview.DefaultLabelFormatter;
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.BarGraphSeries;
 import com.jjoe64.graphview.series.DataPoint;
@@ -29,16 +30,31 @@ public class GraphActivity extends AppCompatActivity {
         //broken
         int[] months = new int[12];//new int[12 * (SightingListContainer.endYear - SightingListContainer.startYear) + SightingListContainer.endMonth - SightingListContainer.startMonth];
 
+        //adds sightings to buckets
         for (Calendar sighting : SightingListContainer.reports) {
-            Log.d("DataAdded", "gotHere with " + sighting.get(Calendar.MONTH));
+            Log.d("sightingTime", sighting.getTime() + "");
             int bucket = sighting.get(Calendar.MONTH);//sighting.get(Calendar.MONTH) - SightingListContainer.startMonth + 12 * (sighting.get(Calendar.YEAR) - SightingListContainer.startYear);
             months[bucket] = months[bucket] + 1;
         }
 
+        //adds buckets to graph
         for (int i = 0; i < months.length; i++) {
             Log.d("DataAdded", "i: " + i + " months[i]" + months[i]);
             series.appendData(new DataPoint(i, months[i]), true, months.length);
         }
+
+        final String[] monthArray = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
+
+        graph.getGridLabelRenderer().setLabelFormatter(new DefaultLabelFormatter() {
+            @Override
+            public String formatLabel(double value, boolean isValueX) {
+                if (isValueX) {
+                    // show normal x values
+                    return monthArray[(int) value];
+                }
+                return value + "";
+            }
+        });
 
         //use http://www.android-graphview.org/bar-chart/ for reference for Bar Graph
         //DataPoint should take format (Date, Sightings)
