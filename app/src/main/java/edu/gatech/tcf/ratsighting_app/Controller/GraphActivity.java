a2013549081396155614069298effada12091666
rbhat35 <rsbhat2@gmail.com>
2017-11-08 21:48:21 -0500

    Working chart w/o labels for months

--- app/src/main/java/edu/gatech/tcf/ratsighting_app/Controller/GraphActivity.java
+++ app/src/main/java/edu/gatech/tcf/ratsighting_app/Controller/GraphActivity.java
@@ -11,8 +11,16 @@ import com.jjoe64.graphview.series.BarGraphSeries;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.HashMap;
+import java.util.List;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
 
 import edu.gatech.tcf.ratsighting_app.Model.RatSighting;
 import edu.gatech.tcf.ratsighting_app.Model.SightingListContainer;
@@ -25,8 +33,8 @@ public class GraphActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_graph);
 
-        GraphView graph = (GraphView) findViewById(R.id.graph);
-        BarGraphSeries<DataPoint> series = new BarGraphSeries<>();
+        LineChart lineChart = (LineChart) findViewById(R.id.chart);
+
         //broken
         int[] months = new int[12];//new int[12 * (SightingListContainer.endYear - SightingListContainer.startYear) + SightingListContainer.endMonth - SightingListContainer.startMonth];
 
@@ -37,6 +45,28 @@ public class GraphActivity extends AppCompatActivity {
             months[bucket] = months[bucket] + 1;
         }
 
+
+        List<Entry> entries = convertDataSetToEntry(months);
+
+        LineDataSet dataset = new LineDataSet(entries, "Month");
+
+        LineData data = new LineData(dataset);
+        dataset.setColors(ColorTemplate.COLORFUL_COLORS); //
+
+        dataset.setDrawFilled(true);
+
+        lineChart.setData(data);
+        lineChart.animateY(5000);
+
+        lineChart.getDescription().setText("Average Calls per Month");
+
+
+
+        /*
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+        BarGraphSeries<DataPoint> series = new BarGraphSeries<>();
+
+
         //adds buckets to graph
         for (int i = 0; i < months.length; i++) {
             Log.d("DataAdded", "i: " + i + " months[i]" + months[i]);
@@ -45,7 +75,6 @@ public class GraphActivity extends AppCompatActivity {
 
         final String[] monthArray = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
 
-        /*
         graph.getGridLabelRenderer().setLabelFormatter(new DefaultLabelFormatter() {
             @Override
             public String formatLabel(double value, boolean isValueX) {
@@ -56,7 +85,7 @@ public class GraphActivity extends AppCompatActivity {
                 return value + "";
             }
         });
-        */
+
 
         //use http://www.android-graphview.org/bar-chart/ for reference for Bar Graph
         //DataPoint should take format (Date, Sightings)
@@ -66,5 +95,16 @@ public class GraphActivity extends AppCompatActivity {
         //Implement logic for filtering date range and having the appropriate graph pop up
 
         graph.addSeries(series);
+        */
+    }
+
+    private List<Entry> convertDataSetToEntry(int[] months) {
+        List<Entry> entries = new ArrayList<>();
+
+        for (int i = 0; i < months.length; i++) {
+            entries.add(new Entry(i, months[i]));
+        }
+
+        return entries;
     }
 }
